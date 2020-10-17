a1c0f4b7c223eaf7728275e104a4c46983830e2f
Achal4004 <51490652+Achal4004@users.noreply.github.com>
2020-10-17 17:20:29 +0530

    Update Implementation.py
    Data Preprocessing done.

--- Implementation.py
+++ Implementation.py
@@ -1,2 +1,23 @@
 # Importing Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
+# Importing the dataset
+train_dataset = pd.read_csv('Training.csv')
+test_dataset = pd.read_csv('Testing.csv')
+
+train_dataset.isnull().sum()
+
+# Slicing and Dicing the dataset to separate features from predictions
+X = train_dataset.iloc[:, 0:132].values
+y = train_dataset.iloc[:, -1].values
+
+# Dimensionality Reduction for removing redundancies
+minimised_dataset = train_dataset.groupby(train_dataset['prognosis']).max()
+minimised_dataset
+
+# Encoding String values to integer constants
+from sklearn.preprocessing import LabelEncoder
+labelencoder = LabelEncoder()
+y = labelencoder.fit_transform(y)
